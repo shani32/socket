c0532ba15afe3e97724d760399ad4c7945c4bbd5
shani32 <92324405+shani32@users.noreply.github.com>
2022-04-05 14:17:06 +0300

    Add files via upload
    connect between server and client

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ app.use(cors())
 app.use(express.json())
 // const port= process.env.PORT || 5000
 
-const server=app.listen(3004, ()=>{
-    console.log(`connect to port 3004`)
+const server=app.listen(3000, ()=>{
+    console.log(`connect to port 3000`)
 })
 
 io=socket(server)
@@ -20,6 +20,13 @@ io.on('connection', (socket)=>{
         socket.join(data)
         console.log('User Joined Room' + data)
     })
+
+    socket.on("send_message", (data)=>{
+        console.log(data)
+        socket.to(data.room).emit("receive_message", data.content)
+    })
+
+
     socket.on("disconnect", ()=>{
         console.log("user disconnected")
     })
